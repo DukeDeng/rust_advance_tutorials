a90bc71e23c27f1ecf915188d523be793f296391
samurai <452146589@qq.com>
2024-07-11 20:44:33 +0800

    chore:add source code

--- src/notes/02-closure-once.rs
+++ src/notes/02-closure-once.rs
@@ -10,3 +10,18 @@ fn main() {
     let x = vec![1,2,3];
     fn_once(|z|{z == x.len()});
 }
+
+// simple source code
+// pub trait Fn<Args> : FnMut<Args> {
+//     extern "rust-call" fn call(&self, Args) -> Self::Output;
+// }
+
+// pub trait FnMut<Args> : FnOnce<Args> {
+//     extern "rust-call" fn call_mut(&mut self, Args) -> Self::Output;
+// }
+
+// pub trait FnOnce<Args> {
+//     type Output;
+
+//     extern "rust-call" fn call_once(self, Args) -> Self::Output;
+// }
