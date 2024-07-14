c0f1a1d56f7808497b55d0da8843a7dd422042d9
samurai <452146589@qq.com>
2024-07-14 16:36:37 +0800

    chore:iterator consumer

---
+++ src/notes/03-iterator-cosumer.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    let total :i32 =  v1_iter.sum();
+    println!("The sum is: {}", total);
+
+    assert_eq!(total, 6);
+
+    // v1_iter 是借用了 v1，因此 v1 可以照常使用
+    println!("The vector is: {:?}", v1);
+
+    // 以下代码会报错，因为 `sum` 拿到了迭代器 `v1_iter` 的所有权
+    // println!("{:?}",v1_iter);
+}
